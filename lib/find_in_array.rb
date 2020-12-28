07f8a551d3f33b277f359c1d03024adbb07f1403
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 19:54:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   index = nil
   length = array.length
-  for i in 1...length
-    if i == value_to_find
+  for i in 0...length
+    if array[i] == value_to_find
       index = i
     end
   end
